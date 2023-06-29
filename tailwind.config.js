641074d340a6862cd6ab987865b4c6bd3b31fbc1
Shiomi <shiomi.hoshiya@gmail.com>
2023-06-29 17:10:33 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "shio-pink": "#FFD4CE"
+        "shio-beige": "#C1B6AC"
+        "shio-black": "#506369"
+        "shio-white": "#FDFDFD"
+      }
+    },
+  },
+  plugins: [],
+};
